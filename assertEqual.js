dad49a046bcca8daface8ddeeff7156db579fd00
creynolds8 <cameron8.cr@gmail.com>
2024-04-29 16:54:28 -0700

    created and wrote function to check equality of 2 inputs, now with emojis

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢🟢🟢 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("butts", "butts");
