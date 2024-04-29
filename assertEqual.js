7d47185aea8bc1a542e3d0b7da6853440015d848
Cameron Reynolds <160823012+creynolds8@users.noreply.github.com>
2024-04-29 16:56:30 -0700

    Update assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -10,4 +10,4 @@ const assertEqual = function(actual, expected) {
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(1, 2);
-assertEqual("butts", "butts");
+assertEqual("LHL", "LHL");
