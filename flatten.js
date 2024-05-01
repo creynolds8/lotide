7f896c23da6bb4d324f70c43ae9e6502bcdf5332
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 08:56:14 -0700

    create function to flatten given array, combine with eqArrays to check against expected outcome and print message

--- flatten.js
+++ flatten.js
@@ -1,4 +1,10 @@
-const eqArrays = function (arr1, arr2) {
+//create a function to flatten given arrays, assume only one level of nesting
+const flatten = function(arr) {
+  const flatArr = arr.flat();
+  return flatArr;
+};
+
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
@@ -12,14 +18,10 @@ const eqArrays = function (arr1, arr2) {
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`)
+    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`);
   } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`)
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
-//create a function to flatten given arrays, assume only one level of nesting
-const flatten = function(arr){
 
-}
-
-flatten([1, 2, [3, 4], 5, [6]]); // -> [1, 2, 3, 4, 5, 6]
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
