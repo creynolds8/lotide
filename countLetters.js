fec77df8d61eda7d4ac30d538cf84fa114e7e436
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 15:51:27 -0700

    add test case and combine functions to work together

--- countLetters.js
+++ countLetters.js
@@ -23,7 +23,9 @@ const equalObjects = function(obj1, obj2) {
   const arr2 = Object.keys(obj2);
   if (arr1.length !== arr2.length) {
     return false;
-  } 
+  } else {
+
+  }
 };
 
 const check = {
@@ -33,4 +35,5 @@ const check = {
   o: 1
 };
 
-assertEqual(countLetters('hello'), check);
+assertEqual(equalObjects(countLetters('hello!'), check), true);
+assertEqual(equalObjects(countLetters('hello!'), check), false);
