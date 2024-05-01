16386fb8933dce1f27e423787050da2826de0bc4
creynolds8 <cameron8.cr@gmail.com>
2024-04-30 17:52:47 -0700

    add assertArraysEqual function and additional test cases to ensure original array is unmodified

--- without.js
+++ without.js
@@ -10,4 +10,29 @@ const without = function (source, itemsToRemove) {
   console.log(newArr);
 };
 
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`)
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`)
+  }
+};
+
+
 without([1, 2, 3], [1]);
+
+const words = ["hello", "world", "lighthouse"];
+without(words,["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"])
