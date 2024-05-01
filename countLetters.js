d38aa6294bde911c1352599fa6a68dc71174919d
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 15:39:32 -0700

    finish countLetters function and implement test case

--- countLetters.js
+++ countLetters.js
@@ -7,5 +7,22 @@ const assertEqual = function(actual, expected) {
 };
 
 const countLetters = function(str) {
-
+  const results = {};
+  for (const letter of str) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
 };
+
+const check = {
+  h: 1,
+  e: 1,
+  l: 2,
+  o: 1
+};
+
+assertEqual(countLetters('hello'), check);
