b2c4294c5576211b5c073ca2e603f8c2868b3b34
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 15:59:51 -0700

    add beginning of function to compare objects in their entirety. this is not required by the assignment as i thought but want to make sure i keep code for potential later use

---
+++ eqObjects.js
@@ -0,0 +1,9 @@
+const equalObjects = function(obj1, obj2) {
+  const arr1 = Object.keys(obj1);
+  const arr2 = Object.keys(obj2);
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+
+  }
+};
