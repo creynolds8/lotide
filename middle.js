4cee453c53c7f1f5c6a7f1a7cbe5d4de8e2e1b8c
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 12:43:55 -0700

    add test cases for even and odd arrays w/ length > 2 and develop function to handle those cases

--- middle.js
+++ middle.js
@@ -19,11 +19,25 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 const middle = function(arr) {
+  const input = arr;
   const emptyArr = [];
+  const output = [];
+  const midElement = arr.length / 2;
   if (arr.length === 1 || arr.length === 2) {
   return emptyArr;
+  } else if (arr.length % 2 !== 0) {
+    output.push(input[Math.floor(midElement)]);
+    return output;
+  } else if (arr.length % 2 === 0) {
+    output.push(input[Math.floor(midElement -1)]);
+    output.push(input[Math.floor(midElement)]);
+  return output;
+
   }
+
 };
 
 assertArraysEqual(middle([1]), []);
 assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
