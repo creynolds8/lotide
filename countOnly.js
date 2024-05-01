f3d55240a8c5cd95c738a022cd8159d3df367afd
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 14:47:29 -0700

    final format and lint check

--- countOnly.js
+++ countOnly.js
@@ -10,13 +10,13 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
     if (itemsToCount[item]) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
-  }}
-
+  }
   return results;
 };
 
