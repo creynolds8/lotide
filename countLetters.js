b6325b899daf38ba42483c911e5ea89fc0ec7249
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 15:45:36 -0700

    begin function to check equality of two objects. include check for unequal lengths

--- countLetters.js
+++ countLetters.js
@@ -18,6 +18,14 @@ const countLetters = function(str) {
   return results;
 };
 
+const equalObjects = function(obj1, obj2) {
+  const arr1 = Object.keys(obj1);
+  const arr2 = Object.keys(obj2);
+  if (arr1.length !== arr2.length) {
+    return false;
+  } 
+};
+
 const check = {
   h: 1,
   e: 1,
