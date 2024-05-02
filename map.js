674cd89489628cf69d5cfec8e9b67c283b5181ce
creynolds8 <cameron8.cr@gmail.com>
2024-05-02 15:04:36 -0700

    complete map function logic and add assertArraysEqual and eqArrays functions

--- map.js
+++ map.js
@@ -1,5 +1,30 @@
-const words = ["ground", "control", "to", "major", "tom"];
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`)
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`)
+  }
+};
 
 const map = function(arr, callback) {
-
+  const results = [];
+  for (let item of arr) {
+    results.push(callback(item));
+  }
+  return results;
 };
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, (word) => word[0]);
+console.log(results1);
