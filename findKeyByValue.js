4e24fb841020d0f8ce82a5f87a4b578ee6503800
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 17:57:21 -0700

    add basic test case from Compass

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,3 +5,15 @@ const assertEqual = function(actual, expected) {
     console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+Tips
