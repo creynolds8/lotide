bc81acfd9b6941f574a372f7225bb0df57b4dab9
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 21:19:10 -0700

    add eqObject function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,18 @@
+const eqObjects = function(obj1, obj2) {
+  const arr1 = Object.keys(obj1);
+  const arr2 = Object.keys(obj2);
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (const key of arr1) {
+      if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+        if (!eqArrays(obj1[key], obj2[key])) {
+          return false;
+        }
+      } else if (obj1[key] !== obj2[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
