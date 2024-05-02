65b454ced71fdfbbaf409bded708fff1ebc2f857
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 19:35:24 -0700

    add additional test case

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -12,7 +12,7 @@ const findKeyByValue = function(obj, str) {
       return key;
     }
   }
-  
+
 };
 
 const bestTVShowsByGenre = {
@@ -23,3 +23,15 @@ const bestTVShowsByGenre = {
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const check1 = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6
+};
+
+assertEqual(findKeyByValue(check1, 2), "b")
+assertEqual(findKeyByValue(check1, 7), undefined)
