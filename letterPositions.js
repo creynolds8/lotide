6c633edd04cc2b3604bcec5dbebca941bf86966c
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 17:40:20 -0700

    add logic to letterPositions function

--- letterPositions.js
+++ letterPositions.js
@@ -1,6 +1,15 @@
 const letterPositions = function(sentence) {
   const results = {};
-  
+  const str = sentence.toLowerCase();
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      if (results[str[i]]) {
+        results[str[i]].push(i);
+      } else {
+        results[str[i]] = [i];
+      }
+    }
+  }
   return results;
 };
 
