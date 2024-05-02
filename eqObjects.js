5caf93a322a3f2f2ab36825c31c33be44997f42f
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 19:48:04 -0700

    add assertEquals function and add test cases

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,12 @@
-const equalObjects = function(obj1, obj2) {
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(obj1, obj2) {
   const arr1 = Object.keys(obj1);
   const arr2 = Object.keys(obj2);
   if (arr1.length !== arr2.length) {
@@ -7,3 +15,20 @@ const equalObjects = function(obj1, obj2) {
 
   }
 };
+
+const shirtObject = {
+  colour: 'red',
+  size: 'medium'
+};
+const anotherShirtObject = {
+  colour: 'red',
+  size: 'medium'
+};
+const longSleeveShirtObject = {
+  colour: 'red',
+  size: 'medium',
+  sleeveLength: 'long'
+};
+
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
