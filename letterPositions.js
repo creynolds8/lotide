24ccff9bd023664e09adbe6020d3c194effd5a06
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 17:50:05 -0700

    final format and lint check

--- letterPositions.js
+++ letterPositions.js
@@ -13,7 +13,7 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
@@ -27,9 +27,9 @@ const eqArrays = function (arr1, arr2) {
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`)
+    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`);
   } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`)
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
 
