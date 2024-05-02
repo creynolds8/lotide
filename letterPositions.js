f69a3cb4e32fb3e375508f1e57ee6ce908e951a7
creynolds8 <cameron8.cr@gmail.com>
2024-05-01 17:27:16 -0700

    add eqArrays and assertArraysEqual functions

--- letterPositions.js
+++ letterPositions.js
@@ -3,3 +3,23 @@ const letterPositions = function(sentence) {
 
   return results;
 };
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`)
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`)
+  }
+};
