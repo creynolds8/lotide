d3c70ec3f0951ef8bf6ea224317c2511411f157c
creynolds8 <cameron8.cr@gmail.com>
2024-05-02 18:18:34 -0700

    create findKey function and restructure test case

--- findKey.js
+++ findKey.js
@@ -6,16 +6,21 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const findKey = function(obj, callback) {
+  for (key in obj) {
+    if (callback(obj[key])) return key;
+  }
+};
+
+const starObject = {
+  "Blue Hill": { stars: 1 },
+  Akaleri: { stars: 3 },
+  noma: { stars: 2 },
+  elBulli: { stars: 3 },
+  Ora: { stars: 2 },
+  Akelarre: { stars: 3 },
+};
+
+assertEqual(findKey(starObject, (x) => x.stars === 2), 'noma');
 
 
-assertEqual(findKey(
-  {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 },
-  },
-  (x) => x.stars === 2
-), noma)
