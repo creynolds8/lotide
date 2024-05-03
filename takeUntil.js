1d78e5f655b6c02cbfd2848b32f260942be375dd
creynolds8 <cameron8.cr@gmail.com>
2024-05-02 17:47:09 -0700

    add assertArraysEqual and augment test case to use that function

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,23 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`)
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`)
+  }
+};
+
 const takeUntil = function(arr, callback) {
   const output = [];
   for (const e of arr) {
@@ -5,16 +25,17 @@ const takeUntil = function(arr, callback) {
       output.push(e);
     } else {
       break;
-    }}
+    }
+  }
   return output;
 };
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ])
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ])
