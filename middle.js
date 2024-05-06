ee771da92f616ec854cb6da76ed890f71319d80f
creynolds8 <cameron8.cr@gmail.com>
2024-05-06 15:36:15 -0700

    improve function logic

--- middle.js
+++ middle.js
@@ -1,12 +1,12 @@
 const middle = function(arr) {
   const output = [];
   const midElement = arr.length / 2;
-  if (arr.length <= 3) {
+  if (arr.length <= 2) {
     return [];
   } else if (arr.length % 2 !== 0) {
     output.push(arr[Math.floor(midElement)]);
     return output;
-  } else if (arr.length % 2 === 0) {
+  } else {
     output.push(arr[midElement - 1],
       arr[midElement]);
     return output;
