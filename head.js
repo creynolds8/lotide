fb9708b8d1f63422c88b71d5d77890b60fe0cb2a
creynolds8 <cameron8.cr@gmail.com>
2024-05-06 12:28:28 -0700

    remove test functionality add export

--- head.js
+++ head.js
@@ -1,14 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return (`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return (`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(arr) {
   return arr[0];
 };
 
-console.log(assertEqual(head([5, 6, 7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+module.exports = head;
