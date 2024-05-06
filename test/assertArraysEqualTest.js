ba9b7ae9a0d7a2b62afc50040c355ff24b84bc32
creynolds8 <cameron8.cr@gmail.com>
2024-05-06 15:20:39 -0700

    add test case to check false message is working

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,6 @@
 const assertArraysEqual = require('../assertArraysEqual');
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 3, 4], [1, 3, 4]);
+assertArraysEqual([1, 2, 3], [1, 2, 4]);
 assertArraysEqual(['1', 2, 3], ['1', 2, 3]);
 assertArraysEqual(['1', 2, 3], ['1', 2, 3]);
