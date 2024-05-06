364aa25bd5e4f4e1f9c6549b055f6a041f55060b
creynolds8 <cameron8.cr@gmail.com>
2024-05-06 12:49:34 -0700

    format and lint check

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
@@ -9,6 +9,5 @@ const eqArrays = function (arr1, arr2) {
   }
   return true;
 };
-
 
 module.exports = eqArrays;
