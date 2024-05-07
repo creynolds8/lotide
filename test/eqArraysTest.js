4d311acb9f1e0711e1eee82297380e2ba3de1db4
creynolds8 <cameron8.cr@gmail.com>
2024-05-06 21:01:08 -0700

    add test for one empty array and two empty arrays

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -5,7 +5,14 @@ describe('#eqArray', () => {
   it('return false if array lengths are not the same', () =>
     assert.isFalse(eqArrays([1, 2], [1, 2, 3])
     ));
-    it('return false if any elements of the array are not the same', () =>
+  it('return false if any elements of the array are not the same', () =>
     assert.isFalse(eqArrays([1, 2, 3], [1, 2, 4])
     ));
+  it('return false when given one array that is empty', () =>
+    assert.isFalse(eqArrays([1], [])
+    ));
+  it('return true when given two empty arrays', () =>
+    assert.isTrue(eqArrays([], [])
+    ));
+
 });
