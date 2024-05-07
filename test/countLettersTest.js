bb2d386fc72d830dc1463c4d03588eb2a6a8e56d
creynolds8 <cameron8.cr@gmail.com>
2024-05-07 09:16:10 -0700

    add mocha chai tests for 3 scenarios, format and lint check

---
+++ test/countLettersTest.js
@@ -0,0 +1,43 @@
+const assert = require('chai').assert;
+const countLetters = require('../countLetters');
+
+const check1 = {
+  h: 1,
+  e: 1,
+  l: 2,
+  o: 1
+};
+
+const check2 = {
+  t: 1,
+  h: 2,
+  e: 3,
+  o: 2,
+  n: 1,
+  s: 1,
+  w: 1,
+  l: 1,
+  i: 1,
+  v: 1,
+};
+
+const check3 = {
+  H: 1,
+  i: 1,
+  h: 1,
+  e: 1,
+  l: 2,
+  o: 1,
+};
+
+describe('#countLetters', () => {
+  it('return object containing letter keys and number of times seen values for a given string', () => {
+    assert.deepEqual((countLetters('hello')), check1);
+  });
+  it('return object as above but without spaces for multi word strings', () => {
+    assert.deepEqual((countLetters('the ones who live')), check2);
+  });
+  it('return object with case sensitive key value pairs', () => {
+    assert.deepEqual((countLetters('Hi hello')), check3);
+  });
+});
