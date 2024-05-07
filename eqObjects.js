4fe568a499e50fc2c3afde4e6d89ec0d42ee2c77
creynolds8 <cameron8.cr@gmail.com>
2024-05-06 21:20:05 -0700

    remove manual tests and change to require helper functions

--- eqObjects.js
+++ eqObjects.js
@@ -1,22 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const eqObjects = function(obj1, obj2) {
   const arr1 = Object.keys(obj1);
@@ -37,38 +19,5 @@ const eqObjects = function(obj1, obj2) {
   }
 };
 
-const shirtObject = {
-  colour: 'red',
-  size: 'medium'
-};
-const anotherShirtObject = {
-  colour: 'red',
-  size: 'medium'
-};
-const longSleeveShirtObject = {
-  colour: 'red',
-  size: 'medium',
-  sleeveLength: 'long'
-};
-
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
-assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
-
-const multiColourShirtObject = {
-  colours: ['red', 'blue'],
-  size: 'medium'
-};
-const anotherMultiColourShirtObject = {
-  size: 'medium',
-  colours: ['red', 'blue']
-};
-const longSleeveMultiColourShirtObject = {
-  size: 'medium',
-  colours: ['red', 'blue'],
-  sleeveLength: 'long'
-};
-
-assertEqual(eqObjects(multiColourShirtObject, anotherMultiColourShirtObject), true);
-assertEqual(eqObjects(multiColourShirtObject, longSleeveMultiColourShirtObject), false);
 
 module.exports = eqObjects;
