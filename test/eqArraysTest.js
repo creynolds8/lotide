7b816f10713c5aa8ed013cb232b863f81c9b0a63
creynolds8 <cameron8.cr@gmail.com>
2024-05-06 21:37:00 -0700

    add missing braces in it statements

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,19 +2,19 @@ const eqArrays = require('../eqArrays');
 const assert = require('chai').assert;
 
 describe('#eqArray', () => {
-  it('return false if array lengths are not the same', () =>
-    assert.isFalse(eqArrays([1, 2], [1, 2, 3])
-    ));
-  it('return false if any elements of the array are not the same', () =>
-    assert.isFalse(eqArrays([1, 2, 3], [1, 2, 4])
-    ));
-  it('return false when given one array that is empty', () =>
-    assert.isFalse(eqArrays([1], [])
-    ));
-  it('return true when given two empty arrays', () =>
-    assert.isTrue(eqArrays([], [])
-    ));
-  it('return true when arrays are the same length and all elements are equal', () =>
-    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3])
-    ));
+  it('return false if array lengths are not the same', () => {
+    assert.isFalse(eqArrays([1, 2], [1, 2, 3]))
+  });
+  it('return false if any elements of the array are not the same', () => {
+    assert.isFalse(eqArrays([1, 2, 3], [1, 2, 4]))
+  });
+  it('return false when given one array that is empty', () => {
+    assert.isFalse(eqArrays([1], []))
+  });
+  it('return true when given two empty arrays', () => {
+    assert.isTrue(eqArrays([], []))
+  });
+  it('return true when arrays are the same length and all elements are equal', () => {
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]))
+  });
 });
